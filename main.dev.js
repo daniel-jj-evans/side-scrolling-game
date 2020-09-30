247f94bb228ec8dadbe721e39c75530d0d576864
daniel evans <daniel.jj.evans1@gmail.com>
2020-09-30 16:24:12 +0100

    added movemnt feature and artwork

---
+++ main.dev.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var player = document.getElementById("player");
+var container = document.getElementById("gameContainer");
+var moveTop = -230;
+var moveLeft = 0;
+
+function playerMove(e) {
+  if (e.keyCode == 87) {
+    //alert(e.keyCode);
+    moveTop -= 40;
+    player.style.top = moveTop + "px";
+
+    if (moveTop <= -460) {
+      moveTop = -460;
+    }
+  }
+
+  ;
+
+  if (e.keyCode == 83) {
+    moveTop += 40;
+    player.style.top = moveTop + "px";
+
+    if (moveTop >= -140) {
+      moveTop = -140;
+    }
+  }
+
+  ;
+
+  if (e.keyCode == 68) {
+    moveLeft += 20;
+    player.style.left = moveLeft + "px";
+
+    if (moveLeft >= 880) {
+      moveLeft = 880;
+    }
+  }
+
+  ;
+
+  if (e.keyCode == 65) {
+    moveLeft -= 20;
+    player.style.left = moveLeft + "px";
+
+    if (moveLeft <= 20) {
+      moveLeft = 20;
+    }
+  }
+
+  ;
+}
+
+;
+document.addEventListener("keydown", playerMove); //player.addEventListener("keypress",logKey(e));
+//if (e.defaultPrevented) {
+//    return;
+//}
+//switch(e.code){
+//    case "KeyS":
+//        player.style.top = ++100;
+//        break;
+//    
+//};
+//
+//
+//
+//
+//
+//let moveRate = 10;
